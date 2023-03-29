a8c68869271971cb69c9d2566127ff402b6b940c
Maruthi2896 <maruthikj4@gmail.com>
2023-03-29 13:48:43 +0530

    react

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+        <h1> Book store App</h1>
+        <Book/>
     </div>
   );
 }
 
-export default App;
+function Book(){
+  const book={
+    name:"Tha Jungle Book",
+    poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdpPQxGVt_OrUAEbf3qc_o8P5KAmQ9iiS2dA&usqp=CAU",
+    summery:"The stories tell mostly of Mowgli, an Indian boy who is raised by wolves and learns self-sufficiency and wisdom from the jungle animals. The book describes the social life of the wolf pack and, more fancifully, the justice and natural order of life in the jungle."
+ ,rating:9.4
+  };
+
+  return(
+    <div className="book-container"> 
+    <img className="book-poster" src={book.poster} alt={book.name}/>
+    <div className="book-spec">
+       <h2 className="book-name">{book.name}</h2>
+       <p className="book-rating"> ⭐{book.rating}</p>
+      
+    </div>
+    <p className="book-rating">{book.summery}</p>
+    </div>
+  );
+}
+
+export default App
